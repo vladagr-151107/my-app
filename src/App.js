16fff1a03ff31017fd103bb84aad6e30ac03d8da
vladagr-151107 <vladagrushchenko@gmail.com>
2023-02-02 22:05:50 +0200

    improved my code ?

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React, { useState } from "react";
 import Result from "./Result";
 import axios from "axios";
 import "./App.css";
-
 function App() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState("Odesa");
-  const [result, setResult] = useState(null);
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "e43d0522c6a2b491f8bte6b227o4172b";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showTemperature);
-  }
   function showTemperature(response) {
-    setResult({
+    setWeatherData({
+      ready: true,
       city: response.data.city,
       temperature: response.data.temperature.current,
       description: response.data.condition.description,
@@ -22,10 +16,19 @@ function App() {
       icon: response.data.condition.icon,
     });
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function updateCity(event) {
     setCity(event.target.value);
   }
-  if (result) {
+  function search() {
+    let apiKey = "e43d0522c6a2b491f8bte6b227o4172b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+  }
+  if (weatherData.ready) {
     return (
       <div className="container">
         <div className="weather-app-wrapper">
@@ -59,7 +62,7 @@ function App() {
                 </div>
               </div>
             </form>
-            <Result result={result} />
+            <Result result={weatherData} />
             <small>
               <a
                 href="https://github.com/vladagr-151107/vanilla-weather-app"
@@ -82,93 +85,8 @@ function App() {
       </div>
     );
   } else {
-    return (
-      <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <form onSubmit={handleSubmit} className="mb-3">
-              <div className="row">
-                <div className="col-2">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    id="current-location"
-                  >
-                    Current{" "}
-                  </button>
-                </div>
-                <div className="col-8">
-                  <input
-                    type="search"
-                    onChange={updateCity}
-                    placeholder="Search for a city"
-                    className="form-control"
-                    autoComplete="off"
-                  />
-                </div>
-                <div className="col-2">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary"
-                  />
-                </div>
-              </div>
-            </form>
-            <div className="overview">
-              <h1> Odesa </h1>
-              <ul>
-                <li>
-                  <span> Wednesday, Feb 2 13:53</span>
-                </li>
-                <li> Сlear</li>
-              </ul>
-            </div>
-            <div className="row">
-              <div className="col-6">
-                <div className="d-flex weather-temperature">
-                  <img
-                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-                    alt="Clear"
-                  />
-                  <div className="float-left">
-                    <strong id="temperature"> 2</strong>
-                    <span className="units">°C</span>
-                  </div>
-                </div>
-              </div>
-              <div classname="col-6">
-                <ul>
-                  <li>
-                    Humidity: <span></span>67%
-                  </li>
-                  <li>
-                    Wind: <span></span>9 km/h
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <small>
-            <a
-              href="https://github.com/vladagr-151107/vanilla-weather-app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open-source code
-            </a>{" "}
-            by{" "}
-            <a
-              href="https://www.instagram.com/_princesska_1511_/?igshid=YmMyMTA2M2Y%3D"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Vlada Grushchenko
-            </a>
-          </small>
-        </div>
-      </div>
-    );
+    search();
+    return "Loading";
   }
 }
 export default App();
